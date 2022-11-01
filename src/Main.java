9c5a12c2c2fa585db64fcfd0710f5c76d2abda02
KLAIIIII <guyyg03546@gmail.com>
2022-11-01 12:08:03 +0700

    Update

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bitwise Operator");
+        int a = 12; // 0000 0101
+        int b = 25; // 0000 0010
+        int c = 35; // 0010 0011
+        int d = 2; // 0000 0010
+        int e = 2; // 0000 0010
+        int f = 8; // 0000 0000
+        int val;
+        String name = "Mark";
+        int age = 25;
+        boolean result = name instanceof String;
+
+
+        val = a&b;
+        System.out.println("Output of Bitwise AND: " + val);
+
+        val = a | b;
+        System.out.println("Output of Bitwise OR: " + val);
+
+        val = a^b;
+        System.out.println("Output of Bitwise XQR: " + val);
+
+        val = ~c;
+        System.out.println("Output of Bitwise ~: " + val);
+
+        val = d<<8;
+        System.out.println("Output of Bitwise Signed Left Shift: " + val);
+
+        val = e>>3;
+        System.out.println("Output of Bitwise Signed Right Shift: " + val);
+
+        val = f>>>2;
+        System.out.println("Output of Bitwise Unsigned Right Shift: " + val);
+
+        System.out.println("Name us an instance of String: " + result);
+
+    }
+}
